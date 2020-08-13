4261ad319547abeb7c246066b21713187d1058ed
sachin-ee <csachinbe@gmail.com>
2020-08-13 11:12:03 -0700

    Ex 2.52

--- Ex2.44-2.52.rkt
+++ Ex2.44-2.52.rkt
@@ -138,3 +138,23 @@
     (let ((new-painter1 (rotate270 painter1))
           (new-painter2 (rotate270 painter2)))
       (rotate90 ((beside new-painter1 newpainter2) frame)))))
+
+;;; 2.52
+
+;; b.
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+;; c.
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four identity flip-horiz
+                                  flip-vert rotate180)))
+    (combine4 (corner-split painter n))))
